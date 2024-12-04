41ffa3c115db368a6341e21e93d0040d88bb5158
Nick dos Remedios <nick.dosremedios@csiro.au>
2024-12-04 19:50:20 +1100

    Custom user-agent to avoid firewall
    Reverted timeout back to 60

--- tests/test-2-acacia-mill.spec.ts
+++ tests/test-2-acacia-mill.spec.ts
@@ -4,6 +4,8 @@ const searchUrl = 'https://bie-test.ala.org.au/search?q=Acacia&rows=20';
 const taxonId = 'https://id.biodiversity.org.au/taxon/apni/51471290';
 const acaciaUrl = 'https://bie-test.ala.org.au/species/' + taxonId;
 
+test.use({ userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36' });
+
 test('Acacia Mill - names check', async ({ page }) => {
   // Search for Acacia 
   await page.goto(searchUrl);
